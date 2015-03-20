026d51a4bd77ee4fe9215039e9b3db023ad08895
Jen Berry <jenberrymail@gmail.com>
2015-03-20 18:52:56 +0000

    update settings

--- nowhere/settings.py
+++ nowhere/settings.py
@@ -57,16 +57,13 @@ WSGI_APPLICATION = 'nowhere.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': {}
 }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'
 
--- nowhere/settings_dev.py
+++ nowhere/settings_dev.py
@@ -1,1 +1,9 @@
+#settings for the development environemnt, will override settings.py
+
+#must set the DEVELOPMENT environemnt variable
+# $ export DEVELOPMENT=true
+#To unset the environment variable:
+# $ unset DEVELOPMENT
+
+#Turn on debugging
 DEBUG = True
